1e5f7d3dfcf405711fe936b18417f27458f71941
yashwanth11072003 <106366120+yashwanth11072003@users.noreply.github.com>
2024-03-01 23:18:20 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+// ... (previous server code)
+
+// Define routes for /about, /contact, /payment, /book, /login, /signup, and /
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+app.get('/payment', (req, res) => {
+    res.sendFile(path.join(__dirname, 'payment.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'signup.html'));
+});
+
+app.get(['/', '/book'], (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
